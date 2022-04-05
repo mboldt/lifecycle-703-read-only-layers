9b31cbbe8cd83483ed06e8148e3c4de46eeebc4c
Mikey Boldt <mboldt@vmware.com>
2022-04-05 11:57:04 -0500

    Run against file system watcher lifecycle branch
    Signed-off-by: Mikey Boldt <mboldt@vmware.com>

--- main.sh
+++ main.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 thisdir="$(pwd)"
 workdir="${thisdir}/work"
 lifecycledir="${workdir}/lifecycle"
+#lifecyclebranch="703-read-only-layers-perf" # hash directories
+lifecyclebranch="703-read-only-layers-watcher" # file system watcher
 builderdir="${workdir}/tiny-builder"
 appdir="${workdir}/spring-petclinic-rest"
 builder_on="ro-layers-tiny-builder"
@@ -15,9 +17,10 @@ cd "${workdir}"
 
 # Get, test, and build the custom lifeycle
 if [ ! -e lifecycle ]; then
-    git clone --branch 703-read-only-layers-perf git@github.com:mboldt/lifecycle.git "${lifecycledir}"
+    git clone git@github.com:mboldt/lifecycle.git "${lifecycledir}"
 fi
 cd "${lifecycledir}"
+git checkout "${lifecyclebranch}"
 git pull
 #go test -v builder_internal_test.go builder.go logger.go analyzer.go  cache.go exporter.go save.go utils.go
 make clean unit {build,package}-linux-amd64
@@ -44,14 +47,19 @@ git pull
 
 # Build the app with the custom builder
 appimg="spring-petclinic-rest"
+
+for i in $(seq 5); do
 time pack build "${appimg}" \
      --verbose \
      --path "${appdir}" \
      --trust-builder \
      --builder "${builder_on}"
+done
 
-# pack build "$appimg" \
-#      --verbose \
-#      --path "$appdir" \
-#      --trust-builder \
-#      --builder "${builder_off}"
+for i in $(seq 5); do
+time pack build "$appimg" \
+     --verbose \
+     --path "$appdir" \
+     --trust-builder \
+     --builder "${builder_off}"
+done
